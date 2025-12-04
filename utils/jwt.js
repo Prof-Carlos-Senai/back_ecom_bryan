ba91a61e740b18abe1e91f4f5d9034e32bf001c2
Carlos Roberto da Silva Filho <silva@edu.sc.senai.br>
2025-12-04 04:59:52 -0300

    Fix jwt error

--- utils/jwt.js
+++ utils/jwt.js
@@ -4,11 +4,19 @@ const jwt = require('jsonwebtoken')
 const SEGREDO = process.env.JWT_SECRET
 const EXPIRE = process.env.JWT_EXPIRES_IN
 
+// LOGS IMPORTANTES
+console.log("JWT_SECRET carregado:", SEGREDO)
+console.log("JWT_EXPIRES_IN carregado:", EXPIRE)
+
 function gerarToken(payload){
+    console.log("Gerando token com payload:", payload)
+    console.log("Usando segredo:", SEGREDO)
     return jwt.sign(payload, SEGREDO, { expiresIn: EXPIRE})
 }
 
 function verificarToken(token){
+    console.log("Token recebido para verificação:", token)
+    console.log("Segredo usado na verificação:", SEGREDO)
     try{
         return jwt.verify(token,SEGREDO)
     }catch(err){
